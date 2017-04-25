8c6f9117a7b7d1d38933072e2a75efc1ee4a90d9
Sixten Hilborn <hilborn90@gmail.com>
2017-04-25 16:44:22 +0200

    No username in path

--- example2-cross-compile/build_ems.sh
+++ example2-cross-compile/build_ems.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/sixten/emsdk/emsdk_portable/emscripten/master/cmake/Modules/Platform/Emscripten.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=~/emsdk/emsdk_portable/emscripten/master/cmake/Modules/Platform/Emscripten.cmake \
     "$(dirname "$0")"
